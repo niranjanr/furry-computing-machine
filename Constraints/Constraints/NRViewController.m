5c110eb137dea366bfc9a5cac7dfb3b903dd638c
Niranjan Ravichandran <niranjan@asana.com>
2014-01-20 10:34:52 -0800

    Completion of basics in tutorial 2

--- Constraints/Constraints/NRViewController.m
+++ Constraints/Constraints/NRViewController.m
@@ -14,16 +14,14 @@
 
 @implementation NRViewController
 
-- (void)viewDidLoad
+- (IBAction)buttonTapped:(UIButton *)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"X"]) {
+    [sender setTitle:@"A very long title for this button"
+            forState:UIControlStateNormal];
+  } else {
+    [sender setTitle:@"X" forState:UIControlStateNormal];
+  }
 }
 
 @end
