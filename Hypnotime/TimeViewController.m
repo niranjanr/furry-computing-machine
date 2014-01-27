d7e8ed88788812e94c489c4f241d280f0adc8c84
Niranjan R <neeraj.ravichandran@gmail.com>
2014-01-26 22:07:19 -0800

    Add the bounce animation; Page 420

--- Hypnotime/TimeViewController.m
+++ Hypnotime/TimeViewController.m
@@ -18,7 +18,7 @@
     NSLog(@"We are now showing the current time");
 
     // animate and spin the time label
-    [self spinTimeLabel];
+    [self bounceTimeLabel];
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
@@ -55,6 +55,23 @@
 - (void)viewWillDisappear:(BOOL)animated {
     NSLog(@"CurrentTimeViewController will disappear");
     [super viewWillDisappear:animated];
+}
+
+- (void)bounceTimeLabel {
+    CAKeyframeAnimation *bounce = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    CATransform3D forward = CATransform3DMakeScale(1.3, 1.3, 1.0);
+    CATransform3D back = CATransform3DMakeScale(0.7, 0.7, 1.0);
+    CATransform3D forward2 = CATransform3DMakeScale(1.2, 1.2, 1.0);
+    CATransform3D back2 = CATransform3DMakeScale(0.9, 0.9, 1.0);
+    bounce.values = @[[NSValue valueWithCATransform3D:CATransform3DIdentity],
+                      [NSValue valueWithCATransform3D:forward],
+                      [NSValue valueWithCATransform3D:back],
+                      [NSValue valueWithCATransform3D:forward2],
+                      [NSValue valueWithCATransform3D:back2],
+                      [NSValue valueWithCATransform3D:CATransform3DIdentity]];
+    bounce.duration = 0.6;
+
+    [self.timeLabel.layer addAnimation:bounce forKey:@"bounceAnimation"];
 }
 
 - (void)spinTimeLabel {
