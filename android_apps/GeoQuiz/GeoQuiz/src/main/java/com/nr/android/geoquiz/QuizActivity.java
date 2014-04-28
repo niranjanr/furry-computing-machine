d374d9d96f621fa29f1c1d38d15aceb7595bcbd9
Niranjan R <neeraj.ravichandran@gmail.com>
2014-04-27 23:08:44 -0700

    End of Chapter 1

--- android_apps/GeoQuiz/GeoQuiz/src/main/java/com/nr/android/geoquiz/QuizActivity.java
+++ android_apps/GeoQuiz/GeoQuiz/src/main/java/com/nr/android/geoquiz/QuizActivity.java
@@ -1,17 +1,44 @@
 package com.nr.android.geoquiz;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class QuizActivity extends ActionBarActivity {
+
+    private Button mFalseButton;
+    private Button mTrueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 
 
