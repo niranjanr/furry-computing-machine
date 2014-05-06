000ad28503e4966d615976bbeab3050cfca05ee6
Niranjan Ravichandran <niranjan@asana.com>
2014-05-06 16:45:41 -0700

    First commit for CriminalIntent test application

---
+++ android_apps/CriminalIntent/app/src/main/java/com/nr/android/criminalintent/app/CrimeActivity.java
@@ -0,0 +1,11 @@
+package com.nr.android.criminalintent.app;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by niranjanravichandran on 5/6/14.
+ */
+public class CrimeActivity extends FragmentActivity {
+
+}
