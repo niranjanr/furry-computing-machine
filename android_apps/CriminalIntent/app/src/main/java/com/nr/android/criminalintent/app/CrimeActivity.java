cd00c57a7e0adee9129aa1b688fda5a90571e05b
Niranjan Ravichandran <niranjan@asana.com>
2014-05-09 16:21:31 -0700

    Fragments; Page 148

--- android_apps/CriminalIntent/app/src/main/java/com/nr/android/criminalintent/app/CrimeActivity.java
+++ android_apps/CriminalIntent/app/src/main/java/com/nr/android/criminalintent/app/CrimeActivity.java
@@ -1,11 +1,24 @@
 package com.nr.android.criminalintent.app;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 
 /**
  * Created by niranjanravichandran on 5/6/14.
  */
 public class CrimeActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
 
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
 }
